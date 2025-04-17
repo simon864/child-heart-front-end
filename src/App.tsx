5ac03efe194c5fb23bd10afb8790df1cd58758fa
simon864 <katevolved3@mail.ru>
2025-04-17 13:59:39 +0700

    feat: add footer

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import './common.css'
+import Footer from './components/footer/footer'
+
+export default function App() {
+  return (
+    <>
+    <div className='main-content'></div>
+      <Footer></Footer>
+    </>
+  )
+}
