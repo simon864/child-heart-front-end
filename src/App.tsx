dc78feb3c89bdc237e5122f52928daf44a97dbee
Koff Daniil <119159791+II-KoDa-II@users.noreply.github.com>
2025-04-21 21:39:47 +0700

    Feature: Header
    Предсказываю что этот 1% всей страницы займет 99% всех усилий.

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './Common.css'
+import Header from './Components/Header/Header'
+
+function App() {
+  
+
+  return (
+    <>
+    <body>
+
+      <Header />
+      <main className='center-align'>
+      </main>
+
+    </body>
+    </>
+  )
+}
+
+export default App
