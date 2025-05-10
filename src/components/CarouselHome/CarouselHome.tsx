c9a57ea94343762287ab3457e26b3d9a01231767
Koff Daniil <119159791+II-KoDa-II@users.noreply.github.com>
2025-05-10 18:05:29 +0700

    Fix: small adjustments

--- src/components/CarouselHome/CarouselHome.tsx
+++ src/components/CarouselHome/CarouselHome.tsx
@@ -1,6 +1,4 @@
 import Slider from 'react-slick'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 interface CaoruselProps {
   children?: React.ReactNode;
