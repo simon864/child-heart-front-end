0ae54f7f6e4c233edf0a2011998d1088f45f6f03
simon864 <katevolved3@mail.ru>
2025-05-12 16:38:31 +0700

    fix: path photo on HelpNeeded

--- src/pages/HomePage/data.ts
+++ src/pages/HomePage/data.ts
@@ -1,119 +1,140 @@
+import testPhoto from "../../assets/images/test.png";
+import project1 from "../../assets/images/our-projects/project1.png";
+import project2 from "../../assets/images/our-projects/project2.png";
+import project3 from "../../assets/images/our-projects/project3.png";
+import project4 from "../../assets/images/our-projects/project4.png";
+import project5 from "../../assets/images/our-projects/project5.png";
+import project6 from "../../assets/images/our-projects/project6.png";
+import project7 from "../../assets/images/our-projects/project7.png";
+import komarova from "../../assets/images/help-needed/Алина Комарова.png";
+import belishkina from "../../assets/images/help-needed/Анастасия Белышкина.png";
+import chuchalova from "../../assets/images/help-needed/Ксения Чучалова.png";
+
 export const HelpNeeded = [
-    {
-        name : "Алина Комарова",
-        year : 2019,
-        history : "Алина появилась на свет с довольно редкой патологией сердца – в нем всего один желудочек. Подобная аномалия корректируется (mock up text after this point!!!)",
-        toCollect : 108500,
-        collected : 8000,
-        image: "src/assets/images/help-needed/Алина Комарова.png"
-    },
-    {
-        name : "Анастасия Белышкина",
-        year : 2017,
-        history : "Среди диагнозов есть довольно серьезный, требующий неусыпного наблюдения родителей и врачей – врожденный порок сердца, (mock up text after this point!!!)",
-        toCollect : 70000,
-        collected : 20000,
-        image: "src/assets/images/help-needed/Анастасия Белышкина.png"
-    },
-    {
-        name : "Ксения Чучалова",
-        year : 2012,
-        history : "У девочки врожденный порок сердца, осложненный легочной гипертензией. В первый год жизни она перенесла сложную операцию (mock up text after this point!!!)",
-        toCollect : 102000,
-        collected : 1000,
-        image: "src/assets/images/help-needed/Ксения Чучалова.png"
-    },
-    {
-        name : "Иван Иванов",
-        year : 2018,
-        history : "(наполнение 1) Иван появился на свет с довольно редкой патологией сердца – в нем всего один желудочек. Подобная аномалия корректируется (mock up text after this point!!!)",
-        toCollect : 108500,
-        collected : 8000,
-        image: "src/assets/images/test.png"
-    },
-    {
-        name : "Андрей Андреев",
-        year : 2016,
-        history : "(наполнение 2) Среди диагнозов есть довольно серьезный, требующий неусыпного наблюдения родителей и врачей – врожденный порок сердца, (mock up text after this point!!!)",
-        toCollect : 70000,
-        collected : 20000,
-        image: "src/assets/images/test.png"
-    },
-    {
-        name : "Василий Васильев",
-        year : 2015,
-        history : "(наполнение 3) У мальчика врожденный порок сердца, осложненный легочной гипертензией. В первый год жизни он перенес сложную операцию (mock up text after this point!!!)",
-        toCollect : 102000,
-        collected : 1000,
-        image: "src/assets/images/test.png"
-    }
-]
+  {
+    name: "Алина Комарова",
+    year: 2019,
+    history:
+      "Алина появилась на свет с довольно редкой патологией сердца – в нем всего один желудочек. Подобная аномалия корректируется (mock up text after this point!!!)",
+    toCollect: 108500,
+    collected: 8000,
+    image: komarova,
+  },
+  {
+    name: "Анастасия Белышкина",
+    year: 2017,
+    history:
+      "Среди диагнозов есть довольно серьезный, требующий неусыпного наблюдения родителей и врачей – врожденный порок сердца, (mock up text after this point!!!)",
+    toCollect: 70000,
+    collected: 20000,
+    image: belishkina,
+  },
+  {
+    name: "Ксения Чучалова",
+    year: 2012,
+    history:
+      "У девочки врожденный порок сердца, осложненный легочной гипертензией. В первый год жизни она перенесла сложную операцию (mock up text after this point!!!)",
+    toCollect: 102000,
+    collected: 1000,
+    image: chuchalova,
+  },
+  {
+    name: "Иван Иванов",
+    year: 2018,
+    history:
+      "(наполнение 1) Иван появился на свет с довольно редкой патологией сердца – в нем всего один желудочек. Подобная аномалия корректируется (mock up text after this point!!!)",
+    toCollect: 108500,
+    collected: 8000,
+    image: testPhoto,
+  },
+  {
+    name: "Андрей Андреев",
+    year: 2016,
+    history:
+      "(наполнение 2) Среди диагнозов есть довольно серьезный, требующий неусыпного наблюдения родителей и врачей – врожденный порок сердца, (mock up text after this point!!!)",
+    toCollect: 70000,
+    collected: 20000,
+    image: testPhoto,
+  },
+  {
+    name: "Василий Васильев",
+    year: 2015,
+    history:
+      "(наполнение 3) У мальчика врожденный порок сердца, осложненный легочной гипертензией. В первый год жизни он перенес сложную операцию (mock up text after this point!!!)",
+    toCollect: 102000,
+    collected: 1000,
+    image: testPhoto,
+  },
+];
 
 export const OurProjects = [
-    {
-        image: "src/assets/images/our-projects/project1.png"
-    },
-    {
-        image: "src/assets/images/our-projects/project2.png"
-    },
-    {
-        image: "src/assets/images/our-projects/project3.png"
-    },
-    {
-        image: "src/assets/images/our-projects/project4.png"
-    },
-    {
-        image: "src/assets/images/our-projects/project5.png"
-    },
-    {
-        image: "src/assets/images/our-projects/project6.png"
-    },
-    {
-        image: "src/assets/images/our-projects/project7.png"
-    }
-]
+  {
+    image: project1,
+  },
+  {
+    image: project2,
+  },
+  {
+    image: project3,
+  },
+  {
+    image: project4,
+  },
+  {
+    image: project5,
+  },
+  {
+    image: project6,
+  },
+  {
+    image: project7,
+  },
+];
 
 export const Helped = [
-    {
-        name : "Алина Комарова",
-        year : 2019,
-        history : "Алина появилась на свет с довольно редкой патологией сердца – в нем всего один желудочек. Подобная аномалия корректируется (mock up text after this point!!!)",
-        toCollect : 108500,
-        collected : 8000,
-        image: "src/assets/images/help-needed/Алина Комарова.png"
-    },
-    {
-        name : "Анастасия Белышкина",
-        year : 2017,
-        history : "Среди диагнозов есть довольно серьезный, требующий неусыпного наблюдения родителей и врачей – врожденный порок сердца, (mock up text after this point!!!)",
-        toCollect : 70000,
-        collected : 20000,
-        image: "src/assets/images/help-needed/Анастасия Белышкина.png"
-    },
-    {
-        name : "Ксения Чучалова",
-        year : 2012,
-        history : "У девочки врожденный порок сердца, осложненный легочной гипертензией. В первый год жизни она перенесла сложную операцию (mock up text after this point!!!)",
-        toCollect : 102000,
-        collected : 1000,
-        image: "src/assets/images/help-needed/Ксения Чучалова.png"
-    }
-]
+  {
+    name: "Алина Комарова",
+    year: 2019,
+    history:
+      "Алина появилась на свет с довольно редкой патологией сердца – в нем всего один желудочек. Подобная аномалия корректируется (mock up text after this point!!!)",
+    toCollect: 108500,
+    collected: 8000,
+    image: komarova,
+  },
+  {
+    name: "Анастасия Белышкина",
+    year: 2017,
+    history:
+      "Среди диагнозов есть довольно серьезный, требующий неусыпного наблюдения родителей и врачей – врожденный порок сердца, (mock up text after this point!!!)",
+    toCollect: 70000,
+    collected: 20000,
+    image: belishkina,
+  },
+  {
+    name: "Ксения Чучалова",
+    year: 2012,
+    history:
+      "У девочки врожденный порок сердца, осложненный легочной гипертензией. В первый год жизни она перенесла сложную операцию (mock up text after this point!!!)",
+    toCollect: 102000,
+    collected: 1000,
+    image: chuchalova,
+  },
+];
 
 export const News = [
-    {
-        date : "27.02.2025",
-        headline : "Мы выиграли грант!",
-        image: "src/assets/images/test.png"
-    },
-    {
-        date : "31.01.2025",
-        headline : "Алина нуждается в дорогостоящем препарате",
-        image: "src/assets/images/test.png"
-    },
-    {
-        date : "15.01.2025",
-        headline : "Настю ждут в Москве!",
-        image: "src/assets/images/test.png"
-    }
-]
+  {
+    date: "27.02.2025",
+    headline: "Мы выиграли грант!",
+    image: testPhoto,
+  },
+  {
+    date: "31.01.2025",
+    headline: "Алина нуждается в дорогостоящем препарате",
+    image: testPhoto,
+  },
+  {
+    date: "15.01.2025",
+    headline: "Настю ждут в Москве!",
+    image: testPhoto,
+  },
+];
